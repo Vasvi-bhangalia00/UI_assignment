369e7608f9b9fe271be7923d66fccbe8d9a4d017
Vasvi <48618238+Vasvi-bhangalia00@users.noreply.github.com>
2020-08-20 10:26:55 +0530

    Update Hints.js

--- src/components/Hints.js
+++ src/components/Hints.js
@@ -91,40 +91,4 @@ export default function Hints() {
     )
 }
 
-/*
 
-defs : [
-    {
-        word: "",
-        used: bool
-    }
-    {
-        word: "",
-        used: bool
-    }
-    {
-        word: "",
-        used: bool
-    }
-    {
-        word: "",
-        used: bool
-    }
-    {
-        word: "",
-        used: bool
-    }
-]
-
-word: {
-
-},
-
-hints: {
-    defs: [],
-    syn : [],
-    ant: []
-}
-
-
-*/
